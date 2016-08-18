7b95000be58211ca4f1aae8c1c4468015495b2c9
vince.forgetta <forgetta@gmail.com>
2016-08-18 13:48:21 -0400

    Fixed bug that reported NA for lambda.

--- qq.plink.R
+++ qq.plink.R
@@ -5,23 +5,28 @@ args <- commandArgs(trailingOnly = TRUE)
 plink.f = args[1]
 title = args[2]
 
+#plink.f = "P42.txt"
+#title = "Title"
+
 message("Input file = ", plink.f)
 message("Title = ", title)
 
 qq.f = paste(plink.f, ".qq.png", sep="")
 d <- read.table(plink.f, header=T)
 do <- d
+
 obs <- sort(d$P)
+obs <- obs[!is.na(obs)]
 lobs <- -(log10(obs))
 
 exp <- c(1:length(obs)) 
 lexp <- -(log10(exp / (length(exp)+1)))
 
-chisq <- qchisq(1-d$P,1)
+chisq <- qchisq(1-obs,1)
 
 lambda <- round(median(chisq)/qchisq(0.5,1),2)
 
-png(file=qq.f, res=300, height = 2000, width = 2000, antialias = )
+png(file=qq.f, res=300, height = 2000, width = 2000)
 par(mar=c(5,5,4,2))
 plot(0, 0, type="n",	
 	xlim=c(0,max(lexp)), ylim=c(0,max(lobs)), 
@@ -31,6 +36,6 @@ plot(0, 0, type="n",
 	main=title)
 legend("topleft", as.expression(bquote(lambda == .(lambda))), bty='n')
 abline(0,1,col="red",lwd=3, lty=3)
-points(lexp, lobs, cex=1, pch=21, bg="#0000ff33", col="#0000ff", lwd=2)
+points(lexp, lobs, cex=1, pch=21, bg="#0000ff33", col="#0000ff")
 dev.off()
 message("Lambda = ", lambda)
