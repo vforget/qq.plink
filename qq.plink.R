3de17c56a7d03655bdacc9dc7f984396c4ffe2fe
vince.forgetta <forgetta@gmail.com>
2016-08-27 11:59:07 -0400

    Remove underflow obs. Nicer axis labels.

--- qq.plink.R
+++ qq.plink.R
@@ -1,15 +1,16 @@
 #!/usr/bin/Rscript
 
+# Usage qq.plink.R gwas.txt "A plot title"
+# Note: Required that columns name with P values is labeled as 'P'.
+
 args <- commandArgs(trailingOnly = TRUE)
 
 plink.f = args[1]
 title = args[2]
 
-#plink.f = "P42.txt"
-#title = "Title"
-
 message("Input file = ", plink.f)
 message("Title = ", title)
+message("Loading Data")
 
 qq.f = paste(plink.f, ".qq.png", sep="")
 d <- read.table(plink.f, header=T)
@@ -17,6 +18,8 @@ do <- d
 
 obs <- sort(d$P)
 obs <- obs[!is.na(obs)]
+obs <- obs[is.finite(-log10(obs))]
+
 lobs <- -(log10(obs))
 
 exp <- c(1:length(obs)) 
@@ -26,15 +29,19 @@ chisq <- qchisq(1-obs,1)
 
 lambda <- round(median(chisq)/qchisq(0.5,1),2)
 
+message("Drawing Plot")
 png(file=qq.f, res=300, height = 2000, width = 2000)
 par(mar=c(5,5,4,2))
 plot(0, 0, type="n",	
 	xlim=c(0,max(lexp)), ylim=c(0,max(lobs)), 
-	xlab="Expected (-logP)", ylab="Observed (-logP)", 
+	xlab=expression("Expected (-log"[10]*"(P))"),
+	ylab=expression("Observed (-log"[10]*"(P))"),
 	#las=1, xaxs="i", yaxs="i", bty="l",
 	bty='n', 
 	main=title)
-legend("topleft", as.expression(bquote(lambda == .(lambda))), bty='n')
+legend("topleft", c(as.expression(bquote(lambda == .(lambda))),
+		  as.expression(bquote(N == .(length(lobs))))),	
+		  , bty='n')
 abline(0,1,col="red",lwd=3, lty=3)
 points(lexp, lobs, cex=1, pch=21, bg="#0000ff33", col="#0000ff")
 dev.off()
